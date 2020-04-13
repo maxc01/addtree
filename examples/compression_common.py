f39b52f3522c590b58196fe0520b6e7d6e67aa71
maxc01 <xingchen92@gmail.com>
2020-04-13 18:57:07 +0200

    add unique_string utility

--- examples/compression_common.py
+++ examples/compression_common.py
@@ -362,3 +362,37 @@ def get_common_cmd_args():
 
     return args, extra
 
+
+def char_map(index):
+    if index < 26:
+        return index + 97
+    elif index < 52:
+        return index - 26 + 65
+    else:
+        return index - 52 + 48
+
+
+def unique_string(n):
+    """generate unique n-length string
+
+    n: length of string
+    """
+    from functools import reduce
+
+    if n == 0:
+        return ""
+    byte_len = math.ceil(math.log2(52) + math.log2(62) * (n - 1))
+    num = reduce(lambda x, y: x * 256 + y, os.urandom(byte_len), 0)
+    codes = []
+    codes.append(char_map(num % 52))
+    num = math.floor(num / 52)
+    for i in range(1, n):
+        codes.append(char_map(num % 62))
+        num = math.floor(num / 62)
+
+    return "".join(map(chr, codes))
+
+
+def get_experiment_id(n):
+    expid = datetime.now().strftime("%Y-%m-%d-%H:%M:%S-") + unique_string(n)
+    return expid
