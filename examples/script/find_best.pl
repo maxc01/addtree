82f8491f1314a89ecc992c8dc0cdc2aade57c76c
maxc01 <xingchen92@gmail.com>
2020-04-16 00:44:53 +0200

    fix finding best iteration

--- examples/script/find_best.pl
+++ examples/script/find_best.pl
@@ -6,10 +6,19 @@ use feature say;
 
 my $total_res = 0;
 my @vs;
+my @iterations;
 while (<>) {
     $total_res += 1;
     $data = decode_json $_;
     push @vs, -$data->{obj_info}->{value};
+    if (exists $data->{iteration}) {
+    push @iterations, $data->{iteration};
+  } else {
+    push @iterations, $.;
+  }
+
 }
 my $num = max @vs;
+my @argmax = grep $vs[$_]==$num, 0..$#vs;
 say "max is: $num (in $total_res resuls)";
+say "argmax: @iterations[@argmax]";
