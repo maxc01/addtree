d00304043049b561ec2e989c3080f299cca8137a
maxc01 <xingchen92@gmail.com>
2020-04-16 20:34:57 +0200

    change some names

--- examples/smac_utils.py
+++ examples/smac_utils.py
@@ -1,13 +1,13 @@
-from smac_35 import cfg2funcparams_35
-from smac_35 import cs_35
+from smac_cs_40 import cfg2funcparams_40
+from smac_cs_40 import cs_40
 
-from smac_multiple import cfg2funcparams_multiple
-from smac_multiple import cs_multiple
+from smac_cs_multiple import cfg2funcparams_multiple
+from smac_cs_multiple import cs_multiple
 
-from smac_single import cfg2funcparams_single
-# from smac_single import cfg2funcparams_single as cfg2funcparams
-from smac_single import cs_single
-# from smac_single import cs_single as cs
+from smac_cs_single import cfg2funcparams_single
+# from smac_cs_single import cfg2funcparams_single as cfg2funcparams
+from smac_cs_single import cs_single
+# from smac_cs_single import cs_single as cs
 
 
 
