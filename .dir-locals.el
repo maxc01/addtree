4e73c60223751f841c574bd61f3da425b284519e
maxc01 <xingchen92@gmail.com>
2020-08-11 19:13:30 +0200

    remove auto-sync after save

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,15 +2,10 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (eval setenv "PYTHONPATH"
-        (cdr
-         (project-current))))
+  (eval setenv "PYTHONPATH" (cdr (project-current))))
  (python-mode
-  (eglot-workspace-configuration . ((:pyls . (:configurationSources ["flake8"]))))
-  (rsync-remote-base-dir . "VAUXITE2:/esat/vauxite/xma/sing_images/")
-  (eval add-to-list 'python-shell-extra-pythonpaths
-        (cdr
-         (project-current)))
+  ;; (eglot-workspace-configuration . ((:pyls . (:configurationSources ["flake8"]))))
+  ;; (rsync-remote-base-dir . "VAUXITE2:/esat/vauxite/xma/sing_images/")
+  (eval add-to-list 'python-shell-extra-pythonpaths (cdr (project-current)))
   (eval venv-workon "dp")
   (eval message "Now in dp virtualenv")))
-
